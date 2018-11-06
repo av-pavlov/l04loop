66fbe2fdf8474aa472cf350a8c602e9dbb5de910
uufswe21 <sdfauren@gmail.com>
2018-11-06 22:51:55 +0900

    -5

--- L04-05.js
+++ L04-05.js
@@ -1,5 +1,16 @@
-function expDiff(nstr) {
-    // модуль разности между стандартной экспонентой и приближением в виде ряда
+function expDiff(x) {
+    var s = 1;
+	var i = 1;
+	var factI = 1;
+	var elExp = x^i/factI;
+	while (x^i/factI >= 0.0001) {
+		s += usl;
+		i++;
+		factI *= i;
+		elExp = x^i/factI;
+	}
+	modDiff = Math.abs(s - Math.exp(x));
+	return modDiff;
 }
 
 module.exports = expDiff;
