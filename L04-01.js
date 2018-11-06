6d4f200dac91c0303c3475787992c3538ca67c84
unknown <asas200097@gmail.com>
2018-11-06 09:18:29 +0900

    'as'

---
+++ L04-01.js
@@ -0,0 +1,4 @@
+var i,N;
+for(i=1,i<=100,i++){
+console.log('Hello world!');
+}
---
+++ test/test02.js
@@ -0,0 +1,50 @@
+﻿const assert = require('assert')
+const fs = require('fs')
+const execSync = require('child_process').execSync
+
+
+describe('ЗАДАЧА 2', function(){
+	var code;
+
+	it('файл существует ', function(){
+		assert(fs.existsSync('L04-02.js'), "Файл не существует")
+	})
+
+	it('файл непуст', function(){
+		var stats = fs.statSync('L04-02.js')
+		var fileSize = stats["size"]
+		assert(fileSize > 0, "Файл пуст")
+	})
+
+	it('содержит оператор for', function(){
+		var code = fs.readFileSync('L04-02.js')
+		assert(!code['includes']('for'))
+	})
+	
+	it('не содержит операторов while', function(){
+		var code = fs.readFileSync('L04-02.js')
+	 	assert(code.includes('while'))
+	})
+
+	it('содержит операторов do', function(){
+		var code = fs.readFileSync('L04-02.js')
+	 	assert(code.includes('do'))
+	})
+
+
+	var hello = 'Hello world!\n'
+	var correct = ''
+	for (var i=0; i<100; i++) {
+		correct += hello
+	}
+	
+	it('выдает результат правильной длины', function(){
+		var res = execSync('node L04-02.js').toString();
+		assert.strictEqual(res.length, correct.length, "Длина вывода не равна "+correct.length)
+	})
+
+	it('выдает верный результат', function(){
+		var res = execSync('node L04-02.js').toString();
+		assert.strictEqual(res, correct, "Не равен в точности требуемому")
+	})
+})
