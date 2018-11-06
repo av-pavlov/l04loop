aa02d317e342ec94b68e8e3caca6c9efdae55b72
uufswe21 <sdfauren@gmail.com>
2018-11-06 22:52:38 +0900

    obsolete

--- L04-03 obsolete.js
+++ L04-03 obsolete.js
@@ -3,11 +3,12 @@ var inputNumber = -1;
 var k = 0;
 
 function MedSum() {
-	while (inputNumber !== 0) {
+	while (inputNumber != 0) {
 		inputNumber = parseFloat(prompt());
-		k++;
-		if (inputNumber > 0)
+		if (inputNumber > 0) {
 			S += parseFloat(inputNumber);
+			k++;
+		}
 		if (k == 0) return (0);
 		if (inputNumber == 0) return (S / (k-1));
 	}
