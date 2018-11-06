abb4e19f89fddb2dc1a061d46a18a75d1860b301
uufswe21 <sdfauren@gmail.com>
2018-11-06 22:51:03 +0900

    -4 +hugeTest04

--- L04-04.js
+++ L04-04.js
@@ -1,5 +1,31 @@
 function checkCardNumber(nstr) {
-    // проверка номера кредитной карты
+	var currentFigure = 0;
+	var doubleFigure = 0;
+	var evenTest = 0;
+	var evenS = 0;
+	var notevenS = 0;
+	
+	for (i=nstr.length-1;i>=0;i--) {
+		if (nstr.length == 13) {
+			evenTest = 1;
+		}
+		
+		currentFigure = parseInt(nstr[i]);
+		
+		if (i % 2 == evenTest) {
+			doubleFigure = parseInt(2*currentFigure);
+			if (doubleFigure > 9) {
+				doubleFigure = doubleFigure % 10 + parseInt(doubleFigure / 10);
+			}
+			evenS += parseInt(doubleFigure);
+		}
+		else {
+			notevenS += parseInt(currentFigure);	
+			}
+	}
+	if ((evenS + notevenS) % 10 == 0) return 'true';
+	else
+		return 'false';
 }
 
 module.exports = checkCardNumber;
