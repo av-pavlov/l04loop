fbf669adea55d6ae3653d5a24f8fe7c3935d5cb7
Alexander <av-pavlov@me.local>
2018-11-12 14:36:51 +0900

    test matrix

--- test/test03.js
+++ test/test03.js
@@ -3,14 +3,14 @@
 const assert = require('assert');
 const fs = require('fs');
 
-describe('ЗАДАЧА 3', function(){
+describe('ЗАДАЧА 3', function() {
 	var code;
 
-	it('файл существует ', function(){
+	it('файл существует ', function() {
 		assert(fs.existsSync('L04-03.js'), "Файл не существует");
 	});
 
-	it('файл непуст', function(){
+	it('файл непуст', function() {
 		var stats = fs.statSync('L04-03.js');
 		var fileSize = stats["size"];
 		assert(fileSize > 0, "Файл пуст");
@@ -18,21 +18,34 @@ describe('ЗАДАЧА 3', function(){
 
 	const pyramid = require('../L04-03.js');
 
-	it('верно при n=1', function(){
+	it('верно при n=1', function() {
 		assert.strictEqual(
-			pyramid(1).trimRight(), "#", "ошибка при n=1");
+			pyramid(1).trimRight(), "#\n", "ошибка при n=1");
 	});
 
-	it('верно при n=2', function(){
+	it('верно при n=2', function() {
 		assert.strictEqual(
-			pyramid(2).trimRight(), " #\n###", "ошибка при n=2");
-	})
+			pyramid(2).trimRight(), " #\n###\n", "ошибка при n=2");
+	});
 
-	it('верно при n=2', function(){
-		var n = 20;
+	it('верно при n=4', function() {
 		assert.strictEqual(
-			pyramid(n).trimRight().length, n*(3*n+1)/2 - 1,
-			"длина неправильная")
+			pyramid(2).trimRight(), "   #\n  ###\n #####\n#######\n", 
+			"ошибка при n=4"
+		);
+	});
+
+	it('дает строку нужной длины при всех n до 20 включительно', function() {
+		for (var n = 1; n<21; n++) {
+			assert.strictEqual(
+				pyramid(n).trimRight().length, n*(3*n+1)/2 - 1,
+				"неправильная длина строки при n="+n)
+		}
 	});
 	
+	it('дает строку, которая состоит только из решеток, переводов строки и пробелов', function() {
+		for (var n = 1; n<21; n++) {
+			assert(pyramid(n).match('^[#\n ]*$'), "в строке присутствуют недопустимые символы")
+		}
+	});
 });
